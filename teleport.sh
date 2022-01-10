fb52789c26b6e97f8cf4a053aed8576e1317b6dd
Bipin Raut <bipinraut@Bipins-MacBook-Pro.local>
2022-01-10 19:37:55 +0530

    1st push

---
+++ teleport.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo curl -O https://get.gravitational.com/teleport-v7.3.2-linux-amd64-bin.tar.gz
+tar -xzf teleport-v7.3.2-linux-amd64-bin.tar.gz
+cd teleport
+sudo ./install
+teleport --help
+screen --version
